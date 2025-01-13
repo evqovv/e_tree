e9252ed0e31d0181655918840597d25813c237d6
evqovv <virtualforme36@gmail.com>
2025-01-13 12:27:44 +0800

    commit

--- include/tree.hpp
+++ include/tree.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <fast_io.h>
 #include <algorithm>
 #include <cstddef>
 #include <limits>
@@ -8,18 +7,26 @@
 #include <string>
 #include <string_view>
 #include <vector>
+#include <format>
 #include <stdexcept>
+#include <fast_io.h>
 
 namespace evqovv {
 class tree_list {
 public:
     void run(int argc, char **argv) {
         auto tree_path = parse({ argv + 1, argv + argc });
+
+        ::fast_io::perrln(tree_path);
+
         walk(tree_path, "", 1);
+
+        auto to_print = ::std::format("{} {}, {} {}", total_directories, (total_directories == 1 ? "directory" : "directories"), total_files, (total_files == 1 ? "file" : "files"));
+        ::fast_io::perrln(to_print);
     }
 
 private:
-    ::std::filesystem::path parse(::std::vector<::std::string> const &parts) {
+    ::std::string parse(::std::vector<::std::string> const &parts) {
         bool not_set = true;
         ::std::string tree_path;
 
@@ -38,6 +45,13 @@ private:
             } else {
                 if (not_set) {
                     tree_path = parts[i];
+                    if (!::std::filesystem::exists(tree_path)) {
+                        throw ::std::runtime_error("The path doesn't exist.");
+                    }
+                    if (!::std::filesystem::is_directory(tree_path)) {
+                        throw ::std::runtime_error("The specified path is not a directory.");
+                    }
+
                     not_set = false;
                 } else {
                     throw ::std::runtime_error("Parsing failed.");
@@ -56,25 +70,33 @@ private:
         }
 
         for (::std::size_t i = 0; i != entries.size(); ++i) {
-            ::std::string to_print(prefix);
-            to_print += (i == entries.size() - 1 ? "└── " : "├── ");
-            to_print += entries[i].path().filename().string();
-            if (::std::filesystem::is_symlink(entries[i])) {
-                to_print += " -> ";
-                to_print += ::std::filesystem::read_symlink(entries[i].path());
-            }
+            auto to_print = ::std::format("{}{} {}{}",
+                prefix,
+                (i == entries.size() - 1 ? "└──" : "├──"),
+                entries[i].path().filename().string(), (std::filesystem::is_symlink(entries[i]) ?
+                    " -> " + std::filesystem::read_symlink(entries[i].path()).string() :
+                    ""));
 
             ::fast_io::perrln(to_print);
 
-            if (entries[i].is_directory() && !::std::filesystem::is_symlink(entries[i])) {
-                if (cur_depth < depth) {
-                    walk(root / entries[i].path().filename(), prefix + (i == entries.size() - 1 ? "    " : "│   "), cur_depth + 1);
+            if (entries[i].is_directory()) {
+                ++total_directories;
+
+                if (!::std::filesystem::is_symlink(entries[i])) {
+                    if (cur_depth < depth) {
+                        walk(root / entries[i].path().filename(), prefix + (i == entries.size() - 1 ? "    " : "│   "), cur_depth + 1);
+                    }
                 }
+            } else {
+                ++total_files;
             }
         }
     }
 
     bool display_all_files = false;
     unsigned depth = ::std::numeric_limits<unsigned>::max();
+
+    ::std::size_t total_files = 0;
+    ::std::size_t total_directories = 0;
 };
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "tree.hpp"
 
 int main(int argc, char **argv) {
-    ::evqovv::tree_list tl;
-    tl.run(argc, argv);
+    ::evqovv::tree_list tree_list;
+    tree_list.run(argc, argv);
 }
