74aa462afee0e45e7be04d2c300a467e10e87804
evqovv <virtualforme36@gmail.com>
2025-01-16 16:54:47 +0800

    commit

--- include/tree.hpp
+++ include/tree.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <algorithm>
 #include <filesystem>
 #include <format>
 #include <limits>
@@ -8,17 +7,16 @@
 #include <string>
 #include <string_view>
 #include <vector>
+#include <sys/statvfs.h>
 #include <fast_io.h>
 
 namespace evqovv {
 class tree_list {
 public:
   void run(int argc, char **argv) {
-    options.init();
-
     parse({argv + 1, argv + argc});
 
-    for (auto &&tree_path : options.tree_paths) {
+    for (auto &&tree_path : option.tree_paths) {
       ::fast_io::perrln(tree_path);
       walk(tree_path, "", 1);
       ::fast_io::perrln("");
@@ -32,17 +30,6 @@ public:
   }
 
 private:
-  void
-  sort_by_filename(::std::vector<::std::filesystem::directory_entry> &entries) {
-    auto sort_by_filename =
-        [](::std::filesystem::directory_entry const &a,
-           ::std::filesystem::directory_entry const &b) -> bool {
-      return a.path().filename() < b.path().filename();
-    };
-
-    ::std::sort(entries.begin(), entries.end(), sort_by_filename);
-  }
-
   void parse(::std::vector<::std::string_view> const &parts) {
     for (decltype(parts.size()) i = 0; i != parts.size(); ++i) {
       if (parts[i][0] == '-') {
@@ -50,23 +37,23 @@ private:
         for (auto &&ch : parts[i].substr(1)) {
           switch (ch) {
           case 'a':
-            options.display_all_files = true;
+            option.display_all_files = true;
             break;
           case 'd':
-            options.only_display_directories = true;
+            option.only_display_directories = true;
             break;
-          case 'r':
-            options.reverse_order = true;
+          case 'f':
+            option.display_full_path = true;
             break;
-          case 'z':
-            options.display_file_size = true;
+          case 's':
+            option.display_file_size = true;
             break;
           case 'L':
             ++param_cnt;
             if (i + param_cnt >= parts.size()) {
               throw ::std::runtime_error("Missing argument to -L option.");
             }
-            options.max_depth =
+            option.max_depth =
                 ::std::stoull(::std::string(parts[i + param_cnt]));
             break;
           default:
@@ -76,12 +63,12 @@ private:
         }
         i += param_cnt;
       } else {
-        options.tree_paths.emplace_back(parts[i]);
+        option.tree_paths.emplace_back(parts[i]);
       }
     }
 
-    if (options.tree_paths.empty()) {
-      options.tree_paths.emplace_back(".");
+    if (option.tree_paths.empty()) {
+      option.tree_paths.emplace_back(".");
     }
   }
 
@@ -90,24 +77,22 @@ private:
     ::std::vector<::std::filesystem::directory_entry> entries;
 
     for (auto &&entry : ::std::filesystem::directory_iterator(root)) {
-      entries.push_back(entry);
+      if (option.display_all_files ||
+          entry.path().filename().string()[0] != '.') {
+        entries.push_back(entry);
+      }
     }
 
     for (::std::size_t i = 0; i != entries.size(); ++i) {
-      ::std::string to_print =
-          prefix + (i == entries.size() - 1 ? "└── " : "├── ") +
-          entries[i].path().filename().string() +
-          (std::filesystem::is_symlink(entries[i])
-               ? " -> " +
-                     std::filesystem::read_symlink(entries[i].path()).string()
-               : "");
+      auto to_print = concatenate_string(prefix, entries[i].path(),
+                                         i == entries.size() - 1);
       ::fast_io::perrln(to_print);
 
       if (entries[i].is_directory()) {
         ++total_directories;
 
         if (!::std::filesystem::is_symlink(entries[i])) {
-          if (cur_depth < options.max_depth) {
+          if (cur_depth < option.max_depth) {
             walk(root / entries[i].path().filename(),
                  prefix + (i == entries.size() - 1 ? "    " : "│   "),
                  cur_depth + 1);
@@ -124,11 +109,21 @@ private:
                                    bool last) const {
     ::std::string str = prefix + (last ? "└── " : "├── ");
 
-    if (options.display_file_size) {
-      str += ::std::format("[{}] ", ::std::filesystem::file_size(path));
+    if (option.display_file_size) {
+      if (::std::filesystem::is_directory(path)) {
+        struct statvfs fs_info{};
+
+        if (::statvfs(path.c_str(), &fs_info) != 0) {
+          throw ::std::runtime_error(::std::strerror(errno));
+        }
+
+        str += ::std::format("[{}] ", fs_info.f_bsize);
+      } else {
+        str += ::std::format("[{}] ", ::std::filesystem::file_size(path));
+      }
     }
 
-    str += path;
+    str += option.display_full_path ? path : path.filename();
 
     if (::std::filesystem::is_symlink(path)) {
       str += " -> ";
@@ -139,24 +134,14 @@ private:
   }
 
   struct {
-    bool display_all_files;
-    bool display_file_size;
-    bool only_display_directories;
-    bool reverse_order;
+    bool display_all_files = false;
+    bool display_file_size = false;
+    bool display_full_path = false;
+    bool only_display_directories = false;
 
-    unsigned max_depth;
+    unsigned max_depth = ::std::numeric_limits<unsigned>::max();
     ::std::vector<::std::filesystem::path> tree_paths;
-
-    void init() noexcept {
-      display_all_files = false;
-      display_file_size = false;
-      only_display_directories = false;
-      reverse_order = false;
-
-      max_depth = ::std::numeric_limits<unsigned>::max();
-      tree_paths.clear();
-    }
-  } options;
+  } option;
 
   ::std::size_t total_files = 0;
   ::std::size_t total_directories = 0;
